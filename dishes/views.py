061dfbd1f95600e2eeb0601c0eed694eb7accffa
Murthy Valavala <murthy.mailme@gmail.com>
2021-12-02 06:03:23 +0000

    added dishes view, templates

--- dishes/views.py
+++ dishes/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Dish
 
 # Create your views here.
+
+def all_dishes(request):
+    """ A view to show all dishes, including sorting and search queries """
+
+    dishes = Dish.objects.all()
+
+    context = {
+        'dishes': dishes,
+    }
+
+    return render(request, 'dishes/dishes.html', context)

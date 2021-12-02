998b7a7a52821ff99d190985566fc21c820a3dd6
Murthy Valavala <murthy.mailme@gmail.com>
2021-12-02 05:52:01 +0000

    customized admin

--- dishes/admin.py
+++ dishes/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Dish, Category
 
 # Register your models here.
-admin.site.register(Dish)
-admin.site.register(Category)
+
+class DishAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+# Register your models here.
+admin.site.register(Dish, DishAdmin)
+admin.site.register(Category, CategoryAdmin)
 
--- dishes/models.py
+++ dishes/models.py
@@ -2,6 +2,11 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -13,6 +18,10 @@ class Category(models.Model):
 
 
 class Dish(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Dishes'
+
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)

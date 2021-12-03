f22927f66cfead848a4f6c79de49362d13ed65d7
Murthy Valavala <murthy.mailme@gmail.com>
2021-12-03 05:48:09 +0000

    added search functionality

--- dishes/views.py
+++ dishes/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Dish
 
 # Create your views here.
@@ -7,9 +9,21 @@ def all_dishes(request):
     """ A view to show all dishes, including sorting and search queries """
 
     dishes = Dish.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('dishes'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            dishes = dishes.filter(queries)
 
     context = {
         'dishes': dishes,
+        'search_term': query,
     }
 
     return render(request, 'dishes/dishes.html', context)

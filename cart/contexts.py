91325eee7584c554d4105cf3968ce21a5a35f40f
Murthy Valavala <murthy.mailme@gmail.com>
2022-01-02 05:18:52 +0000

    updated context processor for bag app

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,24 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from dishes.models import Dish
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     dish_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        dish = get_object_or_404(Dish, pk=item_id)
+        total += quantity * dish.price
+        dish_count += quantity
+        dish_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'dish': dish,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)

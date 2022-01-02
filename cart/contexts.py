fa0bfbf9a437983bdbf92a4f4a54c710b76f1fa1
Murthy Valavala <murthy.mailme@gmail.com>
2022-01-02 15:43:37 +0000

    finished product size logic

--- cart/contexts.py
+++ cart/contexts.py
@@ -10,15 +10,27 @@ def cart_contents(request):
     dish_count = 0
     cart = request.session.get('cart', {})
 
-    for item_id, quantity in cart.items():
-        dish = get_object_or_404(Dish, pk=item_id)
-        total += quantity * dish.price
-        dish_count += quantity
-        cart_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'dish': dish,
-        })
+    for item_id, item_data in cart.items():
+        if isinstance(item_data, int):
+            dish = get_object_or_404(Dish, pk=item_id)
+            total += item_data * dish.price
+            dish_count += item_data
+            cart_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'dish': dish,
+            })
+        else:
+            dish = get_object_or_404(Dish, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * dish.price
+                dish_count += quantity
+                cart_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'dish': dish,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)

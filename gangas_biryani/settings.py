76a2e5ecf63674cf5239fc9f2afa8ffb3b94c44c
Murthy Valavala <murthy.mailme@gmail.com>
2022-01-04 07:38:20 +0000

    checkout app views and templates part 1

--- gangas_biryani/settings.py
+++ gangas_biryani/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     'dishes',
     'cart',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -58,6 +61,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'gangas_biryani.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -75,6 +80,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]

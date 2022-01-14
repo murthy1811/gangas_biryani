098973e0105e2e99bfa3dc1bf13c9ed0af12e8f2
Murthy Valavala <murthy.mailme@gmail.com>
2022-01-14 17:51:53 +0000

    removed secret key and set debug

--- gangas_biryani/settings.py
+++ gangas_biryani/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#d_hl9=$w@5+^0eh_2d41u&t6%(t-8_2(#s7%xpw9ray-sl)&x'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['gangas-ms4.herokuapp.com', 'localhost']
+
 
 
 # Application definition
